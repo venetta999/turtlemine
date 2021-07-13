6090b6e98eddea83f66b5fb938e57f7843410af3
venetta999 <66510220+venetta999@users.noreply.github.com>
2021-07-13 10:27:10 +0700

    Update turtlemine.sh

--- turtlemine.sh
+++ turtlemine.sh
@@ -1,5 +1,5 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz
-tar -zxf xmrig-6.13.1-linux-x64.tar.gz
+tar -vxf xmrig-6.13.1-linux-x64.tar.gz
 cd xmrig-6.13.1
 ./xmrig -k -a argon2/chukwav2 -o pond.turtleturtle.club:3333 -u TRTLv2qSWhMEvSR7Nztp5vd26N8jRWQDsFtETZqYLzDUhR2Lmg7ysBq6qnZxTjQt5b4iN4J6DVjJaRX5hznbRdFfeYE7bMrhbFR -p x
 while [ 1 ]; do
